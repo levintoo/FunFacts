93ae2311131af3b7e818b4412fc8aa16df3671be
levintoo <tooklevin@gmail.com>
2024-01-27 21:26:43 +0300

    make a utility function to generate random colors

---
+++ app/src/main/java/com/levintoo/funfacts/Utils.kt
@@ -0,0 +1,15 @@
+package com.levintoo.funfacts
+
+import androidx.compose.ui.graphics.Color
+import kotlin.random.Random
+
+object Utils {
+    fun generateRandomColor(): Color {
+        return Color(
+            red = Random.nextFloat(),
+            green = Random.nextFloat(),
+            blue = Random.nextFloat(),
+            alpha = 1f,
+        )
+    }
+}
